e38444d89d69396571aaca32ba30f0e65d696da7
lisy09 <lisy09.thu@gmail.com>
2021-03-19 15:27:45 +0900

    add docs/

--- dev-docs/scripts/vuepress_build.sh
+++ dev-docs/scripts/vuepress_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="$( cd $PARENT_DIR/.. >/dev/null 2>&1 && pwd )"
+readonly PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly ROOT_DIR="$( cd $PARENT_DIR/.. >/dev/null 2>&1 && pwd )"
 
 source $ROOT_DIR/.env
 set -x
+readonly DOCS_DIR=$ROOT_DIR/$GENERATE_DOCS_PATH
 
 instance=$(docker run -d --rm \
     -v ${ROOT_DIR}/${DOCS_RELATIVE_PATH}:${WOKRKING_PACKAGE_PATH}/${DOCS_RELATIVE_PATH} \
@@ -15,4 +16,8 @@ docker exec $instance bash -c "cd ${WOKRKING_PACKAGE_PATH} \
     && npx vuepress build ${DOCS_RELATIVE_PATH}; \
     node ${WOKRKING_PACKAGE_PATH}/offlinify.js ${DOCS_RELATIVE_PATH}"
 
+
 docker stop $instance
+
+rm -rf $DOCS_DIR
+mv $ROOT_DIR/$DOCS_RELATIVE_PATH/.vuepress/dist $DOCS_DIR
